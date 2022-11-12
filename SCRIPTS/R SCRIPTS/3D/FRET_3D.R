cea9baa99bb2a35f7506260b0aa858786bfa43ce
ConstanzaET <96588513+ConstanzaET@users.noreply.github.com>
2022-11-12 09:17:41 -0600

    Add files via upload

---
+++ SCRIPTS/R SCRIPTS/3D/FRET_3D.R
@@ -0,0 +1,77 @@
+# Paqueterías
+library(ggplot2)
+library(plotly)
+
+# Cargar archivo de DELTA FRET
+df_fret_200_delta <- read.csv(file = "C:/Users/HP/Desktop/FRET R SCRIPTS/Datos3D/FRET_delta_200.csv", 
+                              header = TRUE)
+
+# Cargar archivo de datos de CIDER
+df_fret_200_cider <- readxl::read_excel(path = "C:/Users/HP/Desktop/FRET R SCRIPTS/Datos3D/CIDER _Biblioteca.xlsx", 
+                                        sheet = 1, col_names = TRUE)
+
+
+
+# Extraer los biosensores que se han analizado hasta el momento
+temp1 <- df_fret_200_delta$Construct
+temp2 <- as.numeric(substr(df_fret_200_cider$Entry, 7, 9))
+
+# Obtener datos de CIDER de biosensores analizados
+df_fret_200_cider <- df_fret_200_cider[(temp2 %in% temp1), ]
+
+# Eliminar valor de SED1
+df_fret_200_delta <- df_fret_200_delta[!(df_fret_200_delta$Construct == 201), ]
+
+# Juntar data frames
+df_fret_200_delta <- cbind(df_fret_200_delta, df_fret_200_cider)
+
+# Determinar la cantidad de biosensores de respuesta
+# alta, media y baja
+
+# Biosensores de respuesta alta, baja e intermedia 
+# de acuerdo con los cuartiles
+temp_25 <- unname(quantile(df_fret_200_delta$Mean_Delta)[2]) # 25%
+temp_75 <- unname(quantile(df_fret_200_delta$Mean_Delta)[4]) # 75%
+
+# Cuartiles de 25, 50 y 75%
+temp_75_delta <- df_fret_200_delta[df_fret_200_delta$Mean_Delta > temp_75, ] # Alta
+temp_25_delta <- df_fret_200_delta[df_fret_200_delta$Mean_Delta < temp_25, ] # Baja
+temp_50_delta <- df_fret_200_delta[df_fret_200_delta$Mean_Delta <= temp_75 &
+                                     df_fret_200_delta$Mean_Delta >= temp_25, ]
+
+# Agregar columnas de respuesta a cada data frame
+temp_25_delta$Response <- "Low"
+temp_50_delta$Response <- "Medium"
+temp_75_delta$Response <- "High"
+
+# Juntar dataframes anteriores
+df_fret_200_delta <- rbind(temp_25_delta, temp_50_delta, temp_75_delta)
+
+# Convertir columna "Response" a factores
+df_fret_200_delta$Response <- as.factor(df_fret_200_delta$Response)
+
+df_fret_200_delta$κ
+# Graficar datos de acuerdo con la respuesta
+fig3d <- plot_ly(data = df_fret_200_delta, x = ~NCPR, y = ~κ, z = ~Mean_Delta,
+                 color = ~Response, colors = c('red', "blue", "darkorchid"),
+                 marker = list(size = 2.5))
+
+fig3d <- fig3d %>% add_markers()
+
+fig3d <- fig3d %>% layout(scene = list(xaxis = list(title = 'Net Charge Per Residue'),
+                                       yaxis = list(title = 'kappa'),
+                                       zaxis = list(title = "delta FRET")),
+                          paper_bgcolor = 'rgb(243, 243, 243)',
+                          plot_bgcolor = 'rgb(243, 243, 243)')
+
+fig3d
+
+#Guardar grafico
+
+
+
+
+# Valores arriba de SED1
+temp_sed1 <- df_fret_200_delta[(df_fret_200_delta$Construct == 201), ]$Mean_Delta
+temp_all <- df_fret_200_delta[!(df_fret_200_delta$Construct == 201), ]$Mean_Delta
+length(temp_all[(temp_all > temp_sed1)])
