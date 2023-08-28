f0743d1e86effb66d452d5dd86a52f76df743944
Cesar Ponce-Diego <73264249+Kaz-bits@users.noreply.github.com>
2023-08-28 13:12:45 -0600

    Add files via upload

---
+++ SCRIPTS/R SCRIPTS/FRET/FT. diagram_of_states.R
@@ -0,0 +1,36 @@
+# Paqueterías
+library(ggplot2)
+
+# Crear data frames para los grupos del diagrama de estados
+r1 <- data.frame("f1" = c(0, 0.25, 0), "f2" = c(0.25, 0, 0))
+r2 <- data.frame("f1" = c(0.25, 0.35, 0, 0), "f2" = c(0, 0, 0.35, 0.25))
+r3 <- data.frame("f1" = c(0.65, 0.35, 0, 0.35), "f2" = c(0.35, 0.65, 0.35, 0))
+r4 <- data.frame("f1" = c(0.35, 0, 0), "f2" = c(0.65, 1, 0.35))
+r5 <- data.frame("f1" = c(0.65, 0.35, 1), "f2" = c(0.35, 0, 0))
+
+# Crear vector de ejes numéricos
+label_vec <- c("0" = NULL, "0.2" = 0.2, "0.4" = 0.4, "0.6" = 0.6, "0.8" = 0.8, "1.0" = 1.0)
+
+# Crear diagrama de estados
+ggplot() +
+  geom_polygon(data = r1, aes(x = f1, y = f2), fill = "#9FFF40") +
+  geom_polygon(data = r2, aes(x = f1, y = f2), fill = "#6DC694") +
+  geom_polygon(data = r3, aes(x = f1, y = f2), fill = "#408B40") +
+  geom_polygon(data = r4, aes(x = f1, y = f2), fill = "#FF4040") +
+  geom_polygon(data = r5, aes(x = f1, y = f2), fill = "#4040FF") +
+  coord_cartesian(xlim = c(0, 1), ylim = c(0, 1)) +
+  theme_bw() +
+  labs(x = bquote(f["+"]), y = bquote(f["-"])) +
+  theme(axis.title = element_text(size = 14), 
+        axis.text = element_text(size = 12), 
+        panel.grid = element_blank()) +
+  scale_y_continuous(breaks = seq(0, 1, 0.2), expand = c(0, 0)) +
+  scale_x_continuous(breaks = label_vec, expand = c(0, 0))
+
+# Guardar gráfico 
+ggsave(filename = "D:/ME/ALL/Project_IDP_D2P2/PLOTS/LOCALCIDER/diagram_of_staes.pdf", 
+       device = "pdf", width = 6, height = 5, units = "in", dpi = 450)
+
+# Guardar gráfico 
+ggsave(filename = "D:/ME/ALL/Project_IDP_D2P2/PLOTS/LOCALCIDER/diagram_of_staes.png", 
+       device = "png", width = 6, height = 5, units = "in", dpi = 450)
